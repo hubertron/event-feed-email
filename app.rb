29c23745e7be7d6190c6f5fb062afae0a2d425e3
Steven Hubert <steven.hubert@gmail.com>
2017-07-17 15:40:03 -0600

    inline CSS

--- app.rb
+++ app.rb
@@ -6,18 +6,38 @@ require 'date'
 
 class MySinatraApp < Sinatra::Base
 
-  EVENT_URL = 'https://www.steamboat.com/events/feed'
-  TRUSTYOU_URL = 'https://api.trustyou.com/hotels/39aa8f9d-e5d6-4b73-b8e4-3fff35990d25/seal.json'
+  FCFP_TRUSTID = '39aa8f9d-e5d6-4b73-b8e4-3fff35990d25'
+  ZEPHYR_TRUSTID = '43128308-697d-4ac6-be24-4b5b1edfbeec'
+  VINTAGE_TRUSTID = '0b747c76-b4af-4ba4-8940-3c827621c06f'
 
- get '/events' do
+  EVENT_URL = 'https://www.steamboat.com/events/feed'
+  TRUSTYOU_URL = 'https://api.trustyou.com/hotels/'
+
+  TRUST_TYPE = '/seal.json'
+
+  get '/events' do
     @data = JSON.parse(RestClient.get EVENT_URL)
     @events = @data['events'][0]['Events']
     @resortTitle = 'Winter Park Resort'
     erb :event  
   end
 
-  get '/lodging' do
-    @data = JSON.parse(RestClient.get TRUSTYOU_URL)
+  get '/lodging/fcfp' do
+    @data = JSON.parse(RestClient.get TRUSTYOU_URL + FCFP_TRUSTID + TRUST_TYPE)
+    @data = @data['response']
+    @resortTitle = 'Winter Park Resort'
+    erb :reviews  
+  end
+
+  get '/lodging/zephyr' do
+    @data = JSON.parse(RestClient.get TRUSTYOU_URL + ZEPHYR_TRUSTID + TRUST_TYPE)
+    @data = @data['response']
+    @resortTitle = 'Winter Park Resort'
+    erb :reviews  
+  end
+
+    get '/lodging/vintage' do
+    @data = JSON.parse(RestClient.get TRUSTYOU_URL + VINTAGE_TRUSTID + TRUST_TYPE)
     @data = @data['response']
     @resortTitle = 'Winter Park Resort'
     erb :reviews  
